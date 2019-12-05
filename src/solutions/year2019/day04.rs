3a0a7deb2486588276b59ebe527f9f2a28d0c1bb
Tim Vermeulen <tvermeulen@me.com>
2019-12-05 05:11:37 +0100

    Improve 2019/04

--- src/solutions/year2019/day04.rs
+++ src/solutions/year2019/day04.rs
@@ -7,12 +7,16 @@ fn parse(input: &str) -> Range<u32> {
         .unwrap()
 }
 
+fn digits(n: u32) -> [u32; 6] {
+    [n / 100_000, n / 10_000 % 10, n / 1000 % 10, n / 100 % 10, n / 10 % 10, n % 10]
+}
+
 fn part1(range: Range<u32>) -> usize {
     range
         .filter(|&n| {
-            let mut digits = || digits_len(n, 6);
-            let pairs = || digits().zip(digits().skip(1));
-            pairs().all(|(x, y)| x <= y) && pairs().any(|(x, y)| x == y)
+            let digits = digits(n);
+            let pairs = || digits.windows(2).map(|w| [w[0], w[1]]);
+            pairs().all(|[x, y]| x <= y) && pairs().any(|[x, y]| x == y)
         })
         .count()
 }
@@ -20,9 +24,10 @@ fn part1(range: Range<u32>) -> usize {
 fn part2(range: Range<u32>) -> usize {
     range
         .filter(|&n| {
-            let digits = || digits_len(n, 6);
-            let counts = digits().frequencies();
-            digits().zip(digits().skip(1)).all(|(x, y)| x <= y) && counts.values().any(|&x| x == 2)
+            let digits = digits(n);
+            let mut freq = [0; 10];
+            digits.iter().for_each(|&d| freq[d as usize] += 1);
+            freq.iter().any(|&f| f == 2) && digits.windows(2).all(|w| w[0] <= w[1])
         })
         .count()
 }
@@ -34,4 +39,26 @@ async fn test() -> Result<(), InputError> {
     assert_eq!(part1(range.clone()), 1767);
     assert_eq!(part2(range), 1192);
     Ok(())
+}
+
+#[cfg(test)]
+mod benches {
+    extern crate test;
+
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_part1(b: &mut Bencher) {
+        let input = futures::executor::block_on(get_input(2019, 4)).unwrap();
+        let range = parse(&input);
+        b.iter(|| part1(range.clone()));
+    }
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let input = futures::executor::block_on(get_input(2019, 4)).unwrap();
+        let range = parse(&input);
+        b.iter(|| part2(range.clone()));
+    }
 }
