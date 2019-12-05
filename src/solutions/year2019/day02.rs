5da2f99d740a2bfaa36eb9325878f42a359dcff1
Tim Vermeulen <tvermeulen@me.com>
2019-12-05 15:21:19 +0100

    Update 2019/02 to reuse intcode

--- src/solutions/year2019/day02.rs
+++ src/solutions/year2019/day02.rs
@@ -1,66 +1,29 @@
 use super::*;
 
-fn parse(input: &str) -> Vec<usize> {
-    parser::usize().collect_sep_by(comma()).parse_to_end(&input).unwrap()
+fn parser<'a>() -> impl Parser<&'a str, Output = Vec<i32>> {
+    parser::i32().collect_sep_by(comma())
 }
 
-enum Operation {
-    Add,
-    Multiply,
+fn run(mut data: Vec<i32>, noun: i32, verb: i32) -> i32 {
+    data[1] = noun;
+    data[2] = verb;
+    let mut state = intcode::State::new(
+        data,
+        || panic!("unexpected input opcode"),
+        |_| panic!("unexpected output opcode"),
+    );
+    state.run();
+    state.data[0]
 }
 
-impl Operation {
-    fn operate(self, a: usize, b: usize) -> usize {
-        match self {
-            Self::Add => a + b,
-            Self::Multiply => a * b,
-        }
-    }
+fn part1(data: Vec<i32>) -> i32 {
+    run(data, 12, 2)
 }
 
-enum Opcode {
-    Operation(Operation),
-    End,
-}
-
-impl Opcode {
-    fn new(n: usize) -> Self {
-        match n {
-            1 => Self::Operation(Operation::Add),
-            2 => Self::Operation(Operation::Multiply),
-            99 => Self::End,
-            _ => unreachable!(),
-        }
-    }
-}
-
-fn run(mut nums: Vec<usize>, noun: usize, verb: usize) -> usize {
-    nums[1] = noun;
-    nums[2] = verb;
-
-    for address in (0..).step_by(4) {
-        match Opcode::new(nums[address]) {
-            Opcode::Operation(op) => {
-                if let [source1, source2, dest] = nums[address + 1..address + 4] {
-                    nums[dest] = op.operate(nums[source1], nums[source2]);
-                } else {
-                    unreachable!()
-                }
-            }
-            Opcode::End => return nums[0],
-        }
-    }
-    unreachable!()
-}
-
-fn part1(nums: &[usize]) -> usize {
-    run(nums.to_owned(), 12, 2)
-}
-
-fn part2(nums: &[usize]) -> usize {
+fn part2(data: Vec<i32>) -> i32 {
     for noun in 0..100 {
         for verb in 0..100 {
-            if run(nums.to_owned(), noun, verb) == 19_690_720 {
+            if run(data.clone(), noun, verb) == 19_690_720 {
                 return 100 * noun + verb;
             }
         }
@@ -71,8 +34,8 @@ fn part2(nums: &[usize]) -> usize {
 #[async_std::test]
 async fn test() -> Result<(), InputError> {
     let input = get_input(2019, 2).await?;
-    let nums = parse(&input);
-    assert_eq!(part1(&nums), 3_409_710);
-    assert_eq!(part2(&nums), 7912);
+    let data = parser().parse_to_end(&input).unwrap();
+    assert_eq!(part1(data.clone()), 3_409_710);
+    assert_eq!(part2(data), 7912);
     Ok(())
 }
