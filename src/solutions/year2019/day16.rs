f8d67f0f4c7734c84482130535a39d5486d58310
Tim Vermeulen <tvermeulen@me.com>
2019-12-16 17:55:26 +0100

    2019/16 part 2

---
+++ src/solutions/year2019/day16.rs
@@ -0,0 +1,48 @@
+use super::*;
+
+fn parse(input: &str) -> Vec<u32> {
+    input.chars().map(|c| c.to_digit(10).unwrap()).collect()
+}
+
+fn part2(digits: &[u32]) -> u32 {
+    let offset = digits[..7].iter().fold(0, |n, &d| 10 * n + d) as usize;
+    let suffix_len = digits.len() * 10_000 - offset;
+
+    let mut suffix: Vec<_> = digits.iter().copied().rev().cycle().take(suffix_len).collect();
+
+    for _ in 0..100 {
+        let mut prev = suffix[0];
+        for x in &mut suffix[1..] {
+            *x += prev;
+            *x %= 10;
+            prev = *x;
+        }
+    }
+
+    suffix.iter().rev().take(8).fold(0, |n, &d| 10 * n + d)
+}
+
+#[async_std::test]
+async fn test() -> Result<(), InputError> {
+    let input = get_input(2019, 16).await?;
+    let digits = parse(&input);
+    assert_eq!(part2(&digits), 52825021);
+    Ok(())
+}
+
+#[cfg(test)]
+mod benches {
+    extern crate test;
+
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let input = futures::executor::block_on(get_input(2019, 16)).unwrap();
+        b.iter(|| {
+            let digits = parse(&input);
+            part2(&digits)
+        });
+    }
+}
--- src/solutions/year2019/mod.rs
+++ src/solutions/year2019/mod.rs
@@ -15,3 +15,4 @@ mod day12;
 mod day13;
 mod day14;
 mod day15;
+mod day16;
