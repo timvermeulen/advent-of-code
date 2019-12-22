22f4a1bc303b35876493001fbbfb471b36dbb27e
Tim Vermeulen <tvermeulen@me.com>
2019-12-22 19:24:23 +0100

    Fix 2019/11 part 2

--- src/solutions/year2019/day11.rs
+++ src/solutions/year2019/day11.rs
@@ -19,7 +19,7 @@ impl Debug for Color {
 fn part1(memory: Vec<i64>) -> usize {
     let mut bot = Computer::new(memory);
     let mut paint = Grid::new(Color::Black);
-    let mut pos = Pos { x: 0, y: 0 };
+    let mut pos = Pos::origin();
     let mut dir = Dir::North;
 
     loop {
@@ -43,10 +43,10 @@ fn part1(memory: Vec<i64>) -> usize {
     }
 }
 
-fn part2(memory: Vec<i64>) {
+fn part2(memory: Vec<i64>) -> [u8; 8] {
     let mut bot = Computer::new(memory);
     let mut paint = Grid::new(Color::Black);
-    let mut pos = Pos { x: 0, y: 0 };
+    let mut pos = Pos::origin();
     let mut dir = Dir::North;
     paint[pos] = Color::White;
 
@@ -68,7 +68,26 @@ fn part2(memory: Vec<i64>) {
         pos.move_to(dir);
     }
 
-    println!("{:?}", paint);
+    let mut chars = [0; 8];
+
+    for (i, &offset) in [1, 6, 11, 16, 21, 26, 31, 36].iter().enumerate() {
+        let mut pixels = [false; 5];
+        for (i, &(x, y)) in [(2, 0), (3, 0), (1, 3), (3, 3), (3, 5)].iter().enumerate() {
+            pixels[i] = paint[Pos { x: x + offset, y }] == Color::White;
+        }
+        chars[i] = identify(pixels);
+    }
+
+    chars
+}
+
+const CHARS: [u8; 32] = [
+    b'Y', b'C', b' ', b'F', b' ', b'P', b' ', b' ', b' ', b'B', b'U', b'J', b' ', b' ', b' ', b' ',
+    b'L', b' ', b'K', b'E', b' ', b'R', b' ', b'Z', b' ', b'G', b'H', b' ', b' ', b'A', b' ', b' ',
+];
+
+fn identify([a, b, c, d, e]: [bool; 5]) -> u8 {
+    CHARS[a as usize + 2 * b as usize + 4 * c as usize + 8 * d as usize + 16 * e as usize]
 }
 
 #[async_std::test]
@@ -76,7 +95,7 @@ async fn test() -> Result<(), InputError> {
     let input = get_input(2019, 11).await?;
     let memory = intcode::parser().parse_to_end(&input).unwrap();
     assert_eq!(part1(memory.clone()), 1883);
-    part2(memory);
+    assert_eq!(part2(memory), *b"APUGURFH");
     Ok(())
 }
 
