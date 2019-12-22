ca654c1ee4b77663d7765d0300f9a898f7e678f6
Tim Vermeulen <tvermeulen@me.com>
2019-12-22 19:24:35 +0100

    Add 2019/16 part 1

--- src/solutions/year2019/day16.rs
+++ src/solutions/year2019/day16.rs
@@ -4,6 +4,20 @@ fn parse(input: &str) -> Vec<u32> {
     input.chars().map(|c| c.to_digit(10).unwrap()).collect()
 }
 
+fn part1(digits: &[u32]) -> u32 {
+    let pattern =
+        |i| [0, 1, 0, -1].iter().flat_map(move |&x| iter::repeat(x).take(i + 1)).cycle().skip(1);
+    let mut vec = digits.to_owned();
+    for _ in 0..100 {
+        vec = (0..vec.len())
+            .map(|i| {
+                pattern(i).zip(&vec).map(|(a, &b)| a * b as i32).sum::<i32>().abs() as u32 % 10
+            })
+            .collect();
+    }
+    vec[..8].iter().fold(0, |n, &d| 10 * n + d)
+}
+
 fn part2(digits: &[u32]) -> u32 {
     let offset = digits[..7].iter().fold(0, |n, &d| 10 * n + d) as usize;
     let suffix_len = digits.len() * 10_000 - offset;
@@ -26,7 +40,8 @@ fn part2(digits: &[u32]) -> u32 {
 async fn test() -> Result<(), InputError> {
     let input = get_input(2019, 16).await?;
     let digits = parse(&input);
-    assert_eq!(part2(&digits), 52825021);
+    assert_eq!(part1(&digits), 68_764_632);
+    assert_eq!(part2(&digits), 52_825_021);
     Ok(())
 }
 
